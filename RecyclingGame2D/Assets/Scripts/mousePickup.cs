07dd3c4f302b7cd396502d21a9b04c3cc0f03efa
Ethan Howard <ethanh20009@outlook.com>
2023-01-10 22:39:13 +0000

    Added physics and minigame foundations
    No game made, just drag and drop physics for 2D sorting. Also texturing and lighting

---
+++ RecyclingGame2D/Assets/Scripts/mousePickup.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousePickup : MonoBehaviour
+{
+
+    private float distance = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                pickupable pickupableObject = hit.transform.gameObject.GetComponent<pickupable>();
+                if (pickupableObject != null)
+                {
+                    pickupableObject.pickup(hit.point);
+                }
+            }
+            else
+            {
+                Debug.Log("No hit");
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+        
+}
---
+++ RecyclingGame2D/Assets/pickupable.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupable : MonoBehaviour
+{
+    private TargetJoint2D joint;
+    private bool isHeld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<TargetJoint2D>();
+        joint.enabled = false;
+        isHeld = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHeld)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                isHeld = false;
+                joint.enabled = false;
+                return;
+            }
+            joint.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    public void pickup(Vector2 localCoord)
+    {
+        Debug.Log(gameObject.name + " Has been picked up at");
+        Debug.Log(transform.InverseTransformPoint(localCoord));
+        joint.anchor = transform.InverseTransformPoint(localCoord);
+        isHeld = true;
+        joint.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        joint.enabled = true;
+    }
+}
