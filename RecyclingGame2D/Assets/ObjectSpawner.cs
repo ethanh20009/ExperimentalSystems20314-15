2e72bc49cda3fd478a7d07a2f487b02e9657af34
Dhatshin Karunamoorthy <dkk33@bath.ac.uk>
2023-02-17 22:51:34 +0000

    object movement,colliders and creation limits

--- RecyclingGame2D/Assets/ObjectSpawner.cs
+++ RecyclingGame2D/Assets/ObjectSpawner.cs
@@ -6,9 +6,15 @@ using System.Collections.Generic;
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject ObjecttoSpawn;
+    public int objects_created = 0;
+    int object_limit = 1;
 
     public void CreateObject()
     {
-        GameObject newObject = Instantiate(ObjecttoSpawn);
+        if (objects_created < object_limit)
+        {
+            GameObject newObject = Instantiate(ObjecttoSpawn, new Vector3(0, -2, 0), Quaternion.identity);
+            objects_created++;
+        }
     }
 }
