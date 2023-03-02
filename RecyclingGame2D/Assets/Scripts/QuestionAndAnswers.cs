7b62500a15e7b58a51932f9e7ac5dadfdae62ed1
jeremyO1 <jeremy.ockenden@hotmail.co.uk>
2023-03-02 17:26:37 +0000

    CSV data retrieve working
    Working CSV retrieve data working data

--- RecyclingGame2D/Assets/Scripts/QuestionAndAnswers.cs
+++ RecyclingGame2D/Assets/Scripts/QuestionAndAnswers.cs
@@ -1,8 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 [System.Serializable]
 public class QuestionAndAnswers
 {
     public string Question;
-    public string[] Answers;
+    //public List<string> Answers;
+    public string[] Answers = new string[4];
+    //public string Answer1;
+    //public string Answer2;
+    //public string Answer3;
+    //public string Answer4;
     public int CorrectAnswer;
 }
+
 
