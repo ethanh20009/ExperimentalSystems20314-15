f05025b369a6e5d2cbf8e80243aa9dd98b106871
jessySodimu <113013386+jessySodimu@users.noreply.github.com>
2023-03-13 13:15:41 +0000

    Update Info.cs

--- RecyclingGame2D/Assets/Scripts/ConveyorBelt/Info.cs
+++ RecyclingGame2D/Assets/Scripts/ConveyorBelt/Info.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class Info : MonoBehaviour
 {
     public GameObject gameRulesPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnMouseHover()
+    GameObject gameRulesScreen = new GameObject();
+    
+    void OnMouseOver()
     {
         GameObject gameRulesScreen = Instantiate(gameRulesPrefab, new Vector2(0, 0), Quaternion.identity);
+    }
+
+    void OnMouseExit()
+    {
+        Destroy(gameRulesScreen);
     }
 }
