238ea3b5b746f044efacb092d1c7c647f0c3ef0d
Dhatshin Karunamoorthy <dkk33@bath.ac.uk>
2023-03-14 21:40:32 +0000

    unit testing

---
+++ RecyclingGame2D/Assets/OvercookedTests/EditMode/MovementTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class MovementTest
+{
+    [Test]
+    public void PressDown()
+    {
+    }
+}
