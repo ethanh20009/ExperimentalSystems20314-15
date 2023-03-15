67584a7a3511c05765b535b0caf7d1f52a2648c1
Ethan Howard <ethanh20009@outlook.com>
2023-03-15 22:47:02 +0000

    Commented CompostTest

--- RecyclingGame2D/Assets/Scripts/CompostTests/PlayMode/CompostPlayTest.cs
+++ RecyclingGame2D/Assets/Scripts/CompostTests/PlayMode/CompostPlayTest.cs
@@ -10,6 +10,7 @@ public class CompostPlayTest
     [OneTimeSetUp]
     public void LoadScene()
     {
+        //Load Compost Minigame Scene to Test
         EditorSceneManager.LoadScene("CompostMinigame");
     }
 
@@ -19,14 +20,25 @@ public class CompostPlayTest
     [UnityTest]
     public IEnumerator testCollision()
     {
+        //Get Game manager component and check if initialised correctly
         CompostGameState cgs = GameObject.FindObjectOfType<CompostGameState>();
         Assert.NotNull(cgs);
+
+        //Get the Starting score (0)
         int score = cgs.score;
+
+        //Find starting compostable item and bin
         CompostItem item = GameObject.FindObjectOfType<CompostItem>();
         compostBinScript bin = GameObject.FindObjectOfType<compostBinScript>();
+
+        //Enforce item is compostable and force collision with bin
         item.isCompostable = true;
         item.transform.position = bin.transform.position;
+
+        //Wait one second for unity's physics engine to detect and handle collision, calling our functions
         yield return new WaitForSeconds(1f);
+
+        //Check if score was successfully increased as a result of the collision
         int newScore = cgs.score;
         Assert.Less(score, newScore);
     }
