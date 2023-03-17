23715b4fccfef9e53975d58fd02a1aec7768029d
Dhatshin Karunamoorthy <dkk33@bath.ac.uk>
2023-03-17 20:14:02 +0000

    added some testing

---
+++ RecyclingGame2D/Assets/OvercookedTests/PlayMode/ObjectCreationTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class CreateObject
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void CreatePaper()
+    {
+        //Paper.onClick.Invoke();
+    }
+    public void CreatePlastic()
+    {
+        //Paper.onClick.Invoke();
+    }
+    public void CreateGlass()
+    {
+        //Paper.onClick.Invoke();
+    }
+}
